c51f37023bb4442ed22bdd887520061375e322cb
matute0 <matifernandezescuder@hotmail.com>
2026-07-30 19:51:33 -0300

    Creating project commit

---
+++ src/main/java/matuteferr/emifer3dcalc/Emifer3dCalcApplication.java
@@ -0,0 +1,13 @@
+package matuteferr.emifer3dcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emifer3dCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Emifer3dCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/matuteferr/emifer3dcalc/Emifer3dCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package matuteferr.emifer3dcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emifer3dCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
